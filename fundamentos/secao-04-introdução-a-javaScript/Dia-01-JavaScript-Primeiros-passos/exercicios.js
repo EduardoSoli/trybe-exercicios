d546f3e6f89e5c35a8b0163b80825d36dd2e9ac0
EduardoSoli <yourn2018@gmail.com>
2022-12-10 15:45:14 -0300

    exercicios 4.1

---
+++ fundamentos/secao-04-introdução-a-javaScript/Dia-01-JavaScript-Primeiros-passos/exercicios.js
@@ -0,0 +1,166 @@
+    // 1
+
+//  const a = 2;
+//  const b = 5;
+
+//  let adicao = a + b;
+//  let subtracao = a - b;
+//  let multiplicacao = a * b;
+//  let divisao = a / b;
+//  let modulo = a % b;
+//  console.log(adicao,subtracao,multiplicacao, divisao,modulo);
+
+
+    // 2
+
+    // const valor1 = 5
+    // const valor2 = 2
+
+    //  if (valor1 > valor2){
+    //     return console.log(valor1);
+    //  }
+    //  else{
+    //     return console.log(valor2);
+    //  }
+
+
+    // 3
+
+    // const valor1 = 5
+    // const valor2 = 2
+    // const valor3 = 9
+
+    // if (valor1 > valor2 && valor1 > valor3){
+    //     return console.log(valor1);
+    // }
+    // else if(valor2 > valor1 && valor2 > valor3){
+    //     return console.log(valor2);
+    // }
+    // else{
+    //     console.log(valor3);
+    // }
+
+    // 4
+
+    // const valor = 8;
+
+    //     if (valor > 0){
+    //         return console.log('positive');
+    //     }
+    //     else if (valor < 0){
+    //         return console.log('negative');
+    //     }
+    //     else{
+    //         return console.log("zero");
+    //     }
+
+    // 5
+
+    // const a = 80;
+    // const b = 20;
+    // const c = 80;
+    // let angulos = a > 0 && b > 0 && c > 0;
+
+    // if (angulos){
+    //     if (a + b + c === 180){
+    //         console.log(true);
+    //      } else {
+    //         return console.log(false);
+    //       };
+    //  } else {
+    //     console.log('valor invalido')};
+
+    // 6
+    // let movimento = "peao";
+    //     switch (movimento.toLowerCase()){
+    //         case 'peao':
+    //             console.log('Duas casas pra frente no primeiro movimento ou uma casa pra fremte');
+    //             break;
+    //         case 'torre':
+    //             console.log('Quantas casas quiser na vertical e horizontal');
+    //             break
+    //         case 'cavalo':
+    //             console.log('Sempre anda 3 casas com movimentos em L');
+    //             break
+    //         case 'bispo':
+    //             console.log('Quantas casas quiser na diagonal');
+    //             break
+    //         case 'rainha':
+    //             console.log('Quantas casas quiser em todas as direçoes');
+    //             break
+    //         case 'rei':
+    //             console.log('Anda uma casa em qualquer direçao');
+    //             break 
+    //         default:
+    //             console.log('Peça invalida')
+    //         };
+
+    // 7
+
+    // const nota = Math.floor(Math.random() *120)
+    // console.log(nota)
+    //     if (nota > 100 || nota < 0){
+    //         console.log('Erro');
+    //     }else if (nota >= 90){
+    //         console.log('Nota A');
+    //     }else if(nota >= 80){
+    //         console.log('Nota B');
+    //     }else if(nota >= 70){
+    //         console.log('Nota C');
+    //     }else if (nota >= 60){
+    //         console.log('Nota D');
+    //     }else if (nota >= 50){
+    //         console.log('Nota E');
+    //     }else if (nota < 50){
+    //         console.log('Nota F');
+    //     }
+
+    // 8
+
+    // const a = Math.floor(Math.random() *10)+1;
+    // const b = Math.floor(Math.random() *10)+1;
+    // const c = Math.floor(Math.random() *10)+1;
+    // console.log(a,b,c);
+
+    //     if (a,b,c %2 === 0){
+    //         console.log(true);
+    //     }
+    //     else{
+    //         console.log(false);
+    //     }
+
+    // 9
+
+    //     const a = Math.floor(Math.random() *10)+1;
+    // const b = Math.floor(Math.random() *10)+1;
+    // const c = Math.floor(Math.random() *10)+1;
+    // console.log(a,b,c);
+
+    //     if (a,b,c %2 !== 0){
+    //         console.log(true);
+    //     }
+    //     else{
+    //         console.log(false);
+    //     }
+
+    // 10 
+
+    // const custo = 10 * 1.2;
+    // const venda = 100;
+    //     console.log(custo)
+    //     if ( custo > 0 || venda > 0){
+    //         let lucro = venda * 1000 - custo * 1000;
+    //         console.log(lucro);
+    //     } else {
+    //         console.log('Erro')
+    //     };
+
+    // 11
+
+    // const salario = 37450
+    //     if (salario > 5189 && salario > 0){
+    //         bruto = salario - 570.88;
+    //         liquido = bruto - (bruto * 0.275 - 869.39)
+    //         console.log(liquido);
+    //     }else {
+    //         console.log('Erro')}
