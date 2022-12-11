af732a779742eaad3b9e51339c32d447de1b4aaa
EduardoSoli <yourn2018@gmail.com>
2022-12-11 20:57:15 -0300

    exercicio 4.2

--- fundamentos/secao-04-introdução-a-javaScript/Dia-02-JavaScript-Array-e-loop-For/exercicios.js
+++ fundamentos/secao-04-introdução-a-javaScript/Dia-02-JavaScript-Array-e-loop-For/exercicios.js
@@ -1,5 +1,74 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+    // 1
+    // for (var i = 0; i < numbers.length; i+=1){
+    //     console.log(numbers[i]);
+    // }
 
-    for (var i = 0; i < numbers.length; i+=1){
-        console.log(numbers[i]);
-    }
+    // 2
+    // let soma = 0;
+    //     for (var i = 0; i < numbers.length; i +=1){
+    //        soma += numbers[i];           
+    //     } 
+    //     console.log(soma);
+
+    // 3
+    // let soma = 0;
+    //     for (var i = 0; i < numbers.length; i+=1){
+    //         soma += numbers[i];            
+    //     }
+    //     console.log(soma / 10)
+
+    // 4
+    // let soma = 0;
+    //     for (var i = 0; i < numbers.length; i+=1){
+    //         soma += numbers[i];            
+    //     }
+    //     console.log(soma / 10)
+    //     if (soma > 20){
+    //         console.log('Valos maior que 20')
+    //     }else {
+    //         console.log('Valor menor que 20')
+    //     }
+
+    // 5
+    // let maior = numbers[0];
+    //     for (var i = 0; i < numbers.length; i+=1){
+    //         if (numbers[i] > maior){
+    //             maior = numbers[i];
+    //         }
+    //     }
+    //     console.log(maior);
+
+    // 6
+    // let valor = 0
+    //     for (var i = 0; i < numbers.length; i+=1){
+    //         if (numbers[i] % 2 !== 0){
+    //             valor += 1;
+    //         }
+    //     }
+    //     if (valor === 0){
+    //         console.log('nenhuma valor impar encontrado');
+    //     }else {
+    //         console.log(valor);
+    //     }
+
+    // 7
+        // let menor = numbers[0];
+        // for (var i = 0; i < numbers.length; i+=1){
+        //     if (numbers[i] < menor){
+        //         menor = numbers[i];
+        //     }
+        // }
+        // console.log(menor);
+
+    // 8
+    // let array = [];
+    //     for (var i = 1; i <= 25; i += 1){
+    //         array.push(i);
+    //     }
+    //     console.log(array);
+
+    // // 9
+    //     for (var i = 0; i < array.length; i += 1){
+    //         console.log(array[i] / 2);
+    //     }
