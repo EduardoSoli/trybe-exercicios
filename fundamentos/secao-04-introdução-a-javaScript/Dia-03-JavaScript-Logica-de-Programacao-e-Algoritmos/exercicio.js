5ece66cb0c9834dbefcae4f33ac1aeaaa42b788a
EduardoSoli <yourn2018@gmail.com>
2022-12-11 23:22:54 -0300

    exercicio 4.3

---
+++ fundamentos/secao-04-introdução-a-javaScript/Dia-03-JavaScript-Logica-de-Programacao-e-Algoritmos/exercicio.js
@@ -0,0 +1,19 @@
+    // // 1
+    // let value = 1;
+    //     for (var i = 10; i > 0; i -= 1){
+    //         value *= i;
+    //     }
+    //     console.log(value);
+
+    // 2
+    // let value = 'tryber';
+    // let reverseValue = '';
+    //     for (var i = 0; i < value.length; i += 1){
+    //         reverseValue += value[value.length - 1 - i];
+    //     }
+    //     console.log(reverseValue);
+
+    // 3
+    let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+    
