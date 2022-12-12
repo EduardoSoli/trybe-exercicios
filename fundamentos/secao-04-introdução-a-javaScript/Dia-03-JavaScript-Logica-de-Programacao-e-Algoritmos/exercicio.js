8af0acdd8dd4c457a1646e4148cfd1e402e8d14e
EduardoSoli <yourn2018@gmail.com>
2022-12-12 00:49:32 -0300

    4.3

--- fundamentos/secao-04-introdução-a-javaScript/Dia-03-JavaScript-Logica-de-Programacao-e-Algoritmos/exercicio.js
+++ fundamentos/secao-04-introdução-a-javaScript/Dia-03-JavaScript-Logica-de-Programacao-e-Algoritmos/exercicio.js
@@ -1,6 +1,6 @@
     // // 1
     // let value = 1;
-    //     for (var i = 10; i > 0; i -= 1){
+    //     for (let i = 10; i > 0; i -= 1){
     //         value *= i;
     //     }
     //     console.log(value);
@@ -8,12 +8,42 @@
     // 2
     // let value = 'tryber';
     // let reverseValue = '';
-    //     for (var i = 0; i < value.length; i += 1){
+    //     for (let i = 0; i < value.length; i += 1){
     //         reverseValue += value[value.length - 1 - i];
     //     }
     //     console.log(reverseValue);
 
-    // 3
-    let array = ['java', 'javascript', 'python', 'html', 'css'];
+    // // 3
+    // let array = ['java', 'javascript', 'python', 'html', 'css'];
 
-    
+    // let menor = array[0];
+    // let maior = array[0];
+
+    //     for (let i = 0; i < array.length; i += 1){
+    //         if (array[i].length < menor.length){
+    //             menor = array[i];
+    //         }
+    //     }
+
+    //     for (let i = 0; i < array.length; i += 1){
+    //         if (array[i].length > maior.length){
+    //             maior = array[i];
+    //         }
+    //     }
+    //     console.log(menor , maior);
+
+    // 4
+    // let numeroPrimo = 0;
+        
+    //     for (let i = 2; i <= 50; i += 1){
+    //         let primo = true;
+    //         for (let j = 2; j < i; j += 1){
+    //             if (i % j === 0){
+    //                 primo = false;
+    //             }
+    //         }
+    //         if (primo){
+    //             numeroPrimo = i;
+    //         }
+    //     }
+    //     console.log(numeroPrimo);
