f7d0f72f268975b6601a8823882f43d8e9bb20e4
EduardoSoli <yourn2018@gmail.com>
2022-12-12 15:23:25 -0300

    exercicio 4.4

---
+++ fundamentos/secao-04-introdução-a-javaScript/Dia-04-JavaScript-Objetos-e-funções/script.js
@@ -0,0 +1,30 @@
+    // 1
+    // let name = 'Marta';
+    // let lastName = 'Silva';
+    // let age = 34;
+    // let medals = { golden: 2, silver: 3 };
+
+    let player = {
+        name: 'Maria',
+        lastName: 'Silva',
+        age: 34,
+        medals: {
+            golden: 2,
+            silver: 3,
+        },
+    };
+
+    // // 2
+    // console.log(`A jogadora ${player.name} ${player.lastName} tem ${player.age} anos de idade`);
+
+    // // 3
+    player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]
+        // console.log(player)
+
+    // 4
+    // // player['bestInTheWorld'] = ;
+    //     console.log(`A jogadora ${player['name']} ${player['lastName']} foi eleita a melhor do mundo por ${player['name']} ${player['bestInTheWorld'].length}`)
+
+    // // 5
+    //     console.log(`A jogadora possui ${player.medals.golden} medalhas de ouro e ${player.medals.silver} medalhas de prata}`)
+    
