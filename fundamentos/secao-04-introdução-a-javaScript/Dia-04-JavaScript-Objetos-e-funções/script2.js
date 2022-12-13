6bef76a3f0814ff5bd281775feaab0f3c9069296
EduardoSoli <yourn2018@gmail.com>
2022-12-12 21:25:27 -0300

    exercicio 4.4

---
+++ fundamentos/secao-04-introdução-a-javaScript/Dia-04-JavaScript-Objetos-e-funções/script2.js
@@ -0,0 +1,23 @@
+    // // 1
+    // let names = {
+    //     person1: 'João',
+    //     person2: 'Maria',
+    //     person3: 'Jorge',
+    // };
+    //     for (let hellow in names) {
+    //         console.log(`Olá ${names[hellow]}`);
+    //     }
+
+        // for (let i in names) {
+        //     console.log("Olá" + " " + names[i]);
+        // }
+
+        // 2
+        // let car = {
+        //     model: 'A3 Sedan',
+        //     manufacturer: 'Audi',
+        //     year: 2020
+        //   };
+        //     for (let value in car) {
+        //         console.log(`${value} : ${car[value]}`);
+        //     }
---
+++ fundamentos/secao-04-introdução-a-javaScript/Dia-04-JavaScript-Objetos-e-funções/script3.js
@@ -0,0 +1,73 @@
+    // 1
+        // function soma(a,b){
+        //     return a + b;
+        // };
+        // console.log(soma(5,3));
+
+        // function sub(a,b){
+        //     return a - b;
+        // };
+        // console.log(sub(5,3))
+
+        // function mult(a,b){
+        //     return a * b;
+        // };
+        // console.log(mult(5,3));
+
+        // function div(a,b){
+        //     return a / b;
+        // };
+        // console.log(div(5,3).toFixed(2));
+
+        // function mod(a,b){
+        //     return a % b;
+        // };
+        // console.log(mod(5,3));
+
+    // 2
+        // function higher(a,b){
+        //     if (a > b){
+        //         return a;
+        //     }else{
+        //         return b;
+        //     }
+        // }
+        // console.log(higher(5,3));
+    
+    // 3
+        // function higher(a,b,c){
+        //     if (a > b && a > c){
+        //         return a;
+        //     }else if (b > a && b > c){
+        //         return b;
+        //     }else{
+        //         return c;
+        //     }
+        // };
+        // console.log(higher(5,3,8))
+
+    // 4
+        // function check(a){
+        //     if (a > 0){
+        //         return 'positive';
+        //     }else if (a < 0){
+        //         return 'negative';
+        //     }else{
+        //         return 'zero';
+        //     };
+        // };
+        // console.log(check(Math.floor(Math.random()*20)-10));
+
+    // 5
+        // function ang(a,b,c){
+        //     if (a + b + c > 0){
+        //         if (a + b + c === 180){
+        //             return true;
+        //         }else if (a + b + c !== 180){
+        //             return false;
+        //         }
+        //     }else{
+        //         return "Valor invalido";
+        //     }
+        // }
+        // console.log(ang(20,20,-140))
