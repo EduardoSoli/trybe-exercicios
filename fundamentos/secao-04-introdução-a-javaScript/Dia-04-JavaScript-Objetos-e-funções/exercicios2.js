1f60dab5021a196318d16f1fc090e3121e37c0bc
EduardoSoli <yourn2018@gmail.com>
2022-12-13 10:42:37 -0300

    exercicios 4.4

---
+++ fundamentos/secao-04-introdução-a-javaScript/Dia-04-JavaScript-Objetos-e-funções/exercicios2.js
@@ -0,0 +1,59 @@
+    // 1
+    // function palindromo (string){
+    // //     let reversed = string.split("").reverse().join("");
+    // //     if (string === reversed){
+    // //         return true;
+    // //     }else{
+    // //         return false;
+    // //     }
+    // // }
+
+    //     let length = string.length;
+    //     for (let i = 0; i < length/2; i++) {
+    //      if (string[i] !== string[length - 1 - i]) {
+    //       return false;
+    //      }
+    //     }
+    //     return true;
+    //    }
+    // console.log(palindromo('arara'));
+    // console.log(palindromo('tesouro'));
+
+    // 2
+    // let array = [2, 3, 6, 7, 10, 1];
+    // let maior = array[0];
+    // let indice = 0;
+    
+    // function higher (){
+    //     for (let test = 0; test < array.length; test +=1){
+    //             if (array[test] > maior){
+    //                 maior = array[test];
+    //                 indice = test;
+    //             }
+    //         }
+    //         return indice;
+    //     };
+    //     console.log(higher(array));
+
+    // 3
+    // let array = [2, 4, 6, 7, 10, 0, -3];
+    // let menor = array[0];
+    // let indice = 0;
+    
+    // function lower (){
+    //     for (let test = 0; test < array.length; test +=1){
+    //             if (array[test] < menor){
+    //                 menor = array[test];
+    //                 indice = test;
+    //             }
+    //         }
+    //         return indice;
+    //     };
+    //     console.log(lower(array));
+
+    // 4
+    let array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+        function longer (){
+
+        }
