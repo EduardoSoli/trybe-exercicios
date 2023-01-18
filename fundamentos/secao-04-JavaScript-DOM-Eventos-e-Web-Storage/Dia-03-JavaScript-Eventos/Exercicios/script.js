48383d8c694bf1cb002f88bbb643d42df282073c
EduardoSoli <yourn2018@gmail.com>
2023-01-18 18:10:50 -0300

    exercicios 4.3

--- fundamentos/secao-04-JavaScript-DOM-Eventos-e-Web-Storage/Dia-03-JavaScript-Eventos/Exercicios/script.js
+++ fundamentos/secao-04-JavaScript-DOM-Eventos-e-Web-Storage/Dia-03-JavaScript-Eventos/Exercicios/script.js
@@ -1,17 +1,134 @@
 const createDaysOfTheWeek = () => {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-    const weekDaysList = document.querySelector('.week-days');
-  
-    for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
-    };
+  const weekDays = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ];
+  const weekDaysList = document.querySelector(".week-days");
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement("li");
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
   }
-  
-  createDaysOfTheWeek();
-  
-  // Escreva seu código abaixo.
-  
+};
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+//add Dias do calendario
+const arrayDias = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
+const createCalendar = () => {
+  const selecionaUl = document.getElementById("days");
+
+  for (let i = 0; i < arrayDias.length; i += 1) {
+    let day = arrayDias[i];
+    let criaElementoLi = document.createElement("li");
+    criaElementoLi.innerHTML = day;
+
+    if (day === 24 || day === 31) {
+      criaElementoLi.className = "day holiday";
+      selecionaUl.appendChild(criaElementoLi);
+    } else if (day === 4 || day === 11 || day === 18) {
+      criaElementoLi.className = "day friday";
+      selecionaUl.appendChild(criaElementoLi);
+    } else if (day === 25) {
+      criaElementoLi.className = "day holiday friday";
+      selecionaUl.appendChild(criaElementoLi);
+    } else {
+      criaElementoLi.className = "day";
+      selecionaUl.appendChild(criaElementoLi);
+    }
+  }
+};
+createCalendar();
+
+// feriadoBtn
+const feriadoBtn = (btnName) => {
+  let btnContainer = document.getElementsByClassName("buttons-container")[0];
+  let criaBtn = document.createElement("button");
+  criaBtn.innerHTML = btnName;
+  criaBtn.id = "btn-holiday";
+  btnContainer.appendChild(criaBtn);
+};
+feriadoBtn("Feriado");
+
+// click-event feriadoBtn
+const holidaysCor = () => {
+  const holidayBtn = document.getElementById("btn-holiday");
+  const holidays = document.getElementsByClassName("holiday");
+  let backgroundColor = "rgb(238,238,238)";
+  let newColor = "lightgrey";
+
+  holidayBtn.addEventListener("click", () => {
+    for (let i = 0; i < holidays.length; i += 1) {
+      if (holidays[i].style.backgroundColor === newColor) {
+        holidays[i].style.backgroundColor = backgroundColor;
+      } else {
+        holidays[i].style.backgroundColor = newColor;
+      }
+    }
+  });
+};
+holidaysCor();
+
+// sextaBtn
+const sextaBtn = (btnName) => {
+  let btnContainer = document.getElementsByClassName("buttons-container")[0];
+  let criaBtn = document.createElement("button");
+  criaBtn.innerHTML = btnName;
+  criaBtn.id = "btn-friday";
+  btnContainer.appendChild(criaBtn);
+};
+sextaBtn("Sexta-feira");
+
+// click-event sextaBtn
+const textoSexta = (fridayArray) => {
+  const fridayBtn = document.getElementById("btn-friday");
+  const fridays = document.getElementsByClassName("friday");
+  let newText = "Sextou";
+
+  fridayBtn.addEventListener("click", () => {
+    for (let i = 0; i < fridays.length; i += 1) {
+      if (fridays[i].innerHTML === newText) {
+        fridays[i].innerHTML = newText;
+      } else {
+        fridays[i].innerHTML = fridays[i];
+      }
+    }
+  });
+};
+const sextas = [4, 11, 18, 25];
+textoSexta(sextas);
+
+// mouseover e mouseout
+const zoomOn = (event) => {
+  let days = document.querySelector("#days");
+  days.addEventListener("mouseover", (event) => {
+    event.target.style.fontSize = "30px";
+    event.target.style.fontWeight = "600";
+  });
+};
+
+const zoomOut = (event) => {
+  let days = document.querySelector("#days");
+  days.addEventListener("mouseout", (event) => {
+    event.target.style.fontSize = "20px";
+    event.target.style.fontWeight = "200";
+  });
+};
+zoomOn();
+zoomOut();
+
+const tarefa = (tarefaInput) => {
+    
+}
